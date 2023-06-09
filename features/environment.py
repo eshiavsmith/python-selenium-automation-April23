b769db2cc67706123cf9b0efdcf6c7f876bd22a4
eshiavsmith <eshiavsmith@gmail.com>
2023-06-08 22:55:10 -0400

    updated script to include universal script

--- features/environment.py
+++ features/environment.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-
 
 def browser_init(context):
     """
