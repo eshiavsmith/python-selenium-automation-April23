2dfbef6542197f4423e32ff03422bc7ce8f9b444
eshiavsmith <eshiavsmith@gmail.com>
2023-07-06 22:59:24 -0400

    'added the functions required to reference the page functions'

---
+++ app/application.py
@@ -0,0 +1,16 @@
+from pages.header_page import HeaderPage
+from pages.main_page import MainPage
+from pages.search_results import SearchResultsPage
+from pages.signin_page import SignInPage
+from pages.cart_page import CartPage
+class Application:
+    def __init__(self, driver):
+        self.driver = driver
+        #must provide selenium driver
+
+
+        self.header_page = HeaderPage(self.driver)
+        self.main_page = MainPage(self.driver)
+        self.search_results = SearchResultsPage(self.driver)
+        self.signin_page = SignInPage(self.driver)
+        self.cart_page = CartPage(self.driver)
