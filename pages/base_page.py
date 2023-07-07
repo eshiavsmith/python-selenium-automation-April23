f32c03d23e4bd6bab2392741949aeaa231858d4a
eshiavsmith <eshiavsmith@gmail.com>
2023-07-06 23:00:01 -0400

    'added the functions required for all pages'

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,3 +1,4 @@
+from selenium.webdriver.support import expected_conditions as EC
 class Page:
     def __init__(self, driver):
         self.driver = driver
@@ -9,7 +10,31 @@ class Page:
         self.driver.find_element(*locator).click()
 
     def input_text(self, text, *locator):
-        self.driver.find_element(*locator).send_keys(text)
+        e = self.driver.find_element(*locator)
+        e.clear()
+        e.send_keys(text)
+        print(f' Inputting text: {text}')
 
     def open_url(self, url):
         self.driver.get(url)
+
+    def wait_for_element_click(self, *locator):
+        e = self.wait.until(EC.element_to_be_clickable(locator), message=f'Element not clickable by {locator}')
+        e.click()
+
+    def wait_for_element_disappear(self, *locator):
+        self.wait.until(EC.invisibility_of_element(locator))
+
+    def wait_for_element_appear(self, *locator):
+        return self.wait.until(EC.presence_of_element(locator))
+
+    def verify_element_text(self, expected_text, *locator):
+        actual_text = self.driver.find_element(*locator).text
+        assert expected_text == actual_text, \
+            f'Checking locator {locator}. Expected {expected_text}, but got {actual_text}'
+
+
+    def verify_partial_text(self, expected_text, *locator):
+        actual_text = self.driver.find_element(*locator).text
+        assert expected_text in actual_text, \
+            f'Checking by locator {locator}.  Expected {expected_text}, but got {actual_text}'
