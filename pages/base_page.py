90c1415ac613ac30df554c57a685a1ed6debbe40
eshiavsmith <eshiavsmith@gmail.com>
2023-07-16 21:05:46 -0400

    setup Pycharm project structure

--- pages/base_page.py
+++ pages/base_page.py
@@ -2,9 +2,18 @@ from selenium.webdriver.support import expected_conditions as EC
 class Page:
     def __init__(self, driver):
         self.driver = driver
+        self.wait = WebDriverWait(self.driver, 15)
+        self.base_url = 'https://www.cureskin.com/'
+
+    def open_url(self, url=''):
+        print(f'Opening URL: {url}')
+        self.driver.get(url)
 
     def find_element(self, *locator):
         return self.driver.find_element(*locator)
+
+    def find_elements(self, *locator):
+        return self.driver.find_elements(*locator)
 
     def click(self, *locator):
         self.driver.find_element(*locator).click()
@@ -15,8 +24,7 @@ class Page:
         e.send_keys(text)
         print(f' Inputting text: {text}')
 
-    def open_url(self, url):
-        self.driver.get(url)
+
 
     def wait_for_element_click(self, *locator):
         e = self.wait.until(EC.element_to_be_clickable(locator), message=f'Element not clickable by {locator}')
@@ -33,8 +41,10 @@ class Page:
         assert expected_text == actual_text, \
             f'Checking locator {locator}. Expected {expected_text}, but got {actual_text}'
 
-
     def verify_partial_text(self, expected_text, *locator):
         actual_text = self.driver.find_element(*locator).text
         assert expected_text in actual_text, \
             f'Checking by locator {locator}.  Expected {expected_text}, but got {actual_text}'
+
+  def verify_url_contains_query(self, query):
+        self.wait.until(EC.url_contains(query))
